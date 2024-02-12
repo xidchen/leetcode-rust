98cff7c4f3a1bb803aa07b0dd89a964fb2010351
Sheldon Chen <xidchen.work@gmail.com>
2024-02-12 20:10:08 +0800

    Add problem 2

--- src/leetcode.rs
+++ src/leetcode.rs
@@ -11,3 +11,20 @@ pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     }
     Vec::new()
 }
+
+// 3: /problems/longest-substring-without-repeating-characters/
+pub fn length_of_longest_substring(s: String) -> i32 {
+    let mut last_seen: HashMap<char, i32> = HashMap::new();
+    let mut start:i32 = 0;
+    let mut longest:i32 = 0;
+    for (i, c) in s.chars().enumerate() {
+        if let Some(&last_index) = last_seen.get(&c) {
+            if last_index >= start {
+                start = last_index + 1;
+            }
+        }
+        longest = longest.max(i as i32 - start + 1);
+        last_seen.insert(c, i as i32);
+    }
+    longest
+}
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,18 @@ mod leetcode;
 
 fn main() {
     two_sum();
+    length_of_longest_substring();
 }
 
 fn two_sum() {
     let nums: Vec<i32> = vec![2, 7, 11, 15];
     let target: i32 = 9;
     let result: Vec<i32> = leetcode::two_sum(nums, target);
-    println!("Two sum: {:?}", result)
+    println!("Two sum: {:?}", result);
+}
+
+fn length_of_longest_substring() {
+    let s:String = String::from("abcdefabc");
+    let result: i32 = leetcode::length_of_longest_substring(s);
+    println!("Length of longest substring: {:?}", result);
 }
