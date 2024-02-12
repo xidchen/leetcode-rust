d5dac12160b3e17df5c0873fff111ce22a128d54
Sheldon Chen <xidchen.work@gmail.com>
2024-02-12 21:17:25 +0800

    Use structs

--- src/leetcode.rs
+++ src/leetcode.rs
@@ -1,30 +1,36 @@
 use std::collections::HashMap;
 
-// 1: /problems/two-sum/
-pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    let mut dic: HashMap<i32, usize> = HashMap::new();
-    for (i, &n) in nums.iter().enumerate() {
-        if let Some(&prev_index) = dic.get(&n) {
-            return vec![prev_index as i32, i as i32];
-        }
-        dic.insert(target - n, i);
-    }
-    Vec::new()
-}
+pub struct Leetcode;
 
-// 3: /problems/longest-substring-without-repeating-characters/
-pub fn length_of_longest_substring(s: String) -> i32 {
-    let mut last_seen: HashMap<char, i32> = HashMap::new();
-    let mut start:i32 = 0;
-    let mut longest:i32 = 0;
-    for (i, c) in s.chars().enumerate() {
-        if let Some(&last_index) = last_seen.get(&c) {
-            if last_index >= start {
-                start = last_index + 1;
+impl Leetcode {
+
+    // 1: /problems/two-sum/
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut dic: HashMap<i32, usize> = HashMap::new();
+        for (i, &n) in nums.iter().enumerate() {
+            if let Some(&prev_index) = dic.get(&n) {
+                return vec![prev_index as i32, i as i32];
             }
+            dic.insert(target - n, i);
         }
-        longest = longest.max(i as i32 - start + 1);
-        last_seen.insert(c, i as i32);
+        Vec::new()
     }
-    longest
+
+    // 3: /problems/longest-substring-without-repeating-characters/
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        let mut last_seen: HashMap<char, i32> = HashMap::new();
+        let mut start:i32 = 0;
+        let mut longest:i32 = 0;
+        for (i, c) in s.chars().enumerate() {
+            if let Some(&last_index) = last_seen.get(&c) {
+                if last_index >= start {
+                    start = last_index + 1;
+                }
+            }
+            longest = longest.max(i as i32 - start + 1);
+            last_seen.insert(c, i as i32);
+        }
+        longest
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod leetcode;
 
+use leetcode::Leetcode;
+
 fn main() {
     two_sum();
     length_of_longest_substring();
@@ -8,12 +10,12 @@ fn main() {
 fn two_sum() {
     let nums: Vec<i32> = vec![2, 7, 11, 15];
     let target: i32 = 9;
-    let result: Vec<i32> = leetcode::two_sum(nums, target);
+    let result: Vec<i32> = Leetcode::two_sum(nums, target);
     println!("Two sum: {:?}", result);
 }
 
 fn length_of_longest_substring() {
     let s:String = String::from("abcdefabc");
-    let result: i32 = leetcode::length_of_longest_substring(s);
+    let result: i32 = Leetcode::length_of_longest_substring(s);
     println!("Length of longest substring: {:?}", result);
 }
